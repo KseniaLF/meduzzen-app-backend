c30a048f578286d704e1380cd0f953edd951468a
KseniaLF <llf9391@gmail.com>
2023-12-07 17:47:25 +0200

    fix db.connection file

--- src/config/db.connection.ts
+++ src/config/db.connection.ts
@@ -5,9 +5,9 @@ dotenvConfig({ path: '.env' });
 
 export const CONNECTION: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: `${process.env.POSTGRES_HOST}`,
+  host: process.env.POSTGRES_HOST,
   port: +process.env.POSTGRES_PORT_DB,
-  username: `${process.env.POSTGRES_USER}`,
-  password: `${process.env.POSTGRES_PASSWORD}`,
-  database: `${process.env.POSTGRES_DB}`,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
 };
